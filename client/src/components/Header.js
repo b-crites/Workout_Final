8b6acab147cad8c0232bdee4d3669997a1b6fc2b
Brandon Crites <brandoncrites76@gmail.com>
2023-06-28 11:09:59 -0700

    Added Header files

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './css./Header.css';
+
+export default function Header(){
+    
+    return(
+<div className="navbar-dark">
+<h3>Workout Web</h3>
+
+
+</div>
+
+);
+}
