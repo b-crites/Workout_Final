d1937ff40184446a9142bb06f784086395176278
CollectSpiders <mathewj519@gmail.com>
2023-06-27 21:52:13 -0700

    removed some more redudent files left over from the template

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,35 +3,11 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
   type Category {
     _id: ID
-    name: String
-  }
-
-  type Product {
-    _id: ID
-    name: String
-    description: String
-    image: String
-    quantity: Int
-    price: Float
-    category: Category
-  }
-
-  type Order {
-    _id: ID
-    purchaseDate: String
-    products: [Product]
   }
 
   type User {
     _id: ID
-    firstName: String
-    lastName: String
-    email: String
-    orders: [Order]
-  }
 
-  type Checkout {
-    session: ID
   }
 
   type Auth {
