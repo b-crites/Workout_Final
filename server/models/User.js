99fa06e855a63bf4f327796b371c2c52b991cfbc
CollectSpiders <mathewj519@gmail.com>
2023-07-13 19:01:23 -0700

    added comments to server/models/user.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,8 +1,13 @@
+// Importing the 'mongoose' library for MongoDB object modeling
 const mongoose = require('mongoose');
 
+// Destructuring the 'Schema' object from the mongoose library
 const { Schema } = mongoose;
+
+// Importing the 'bcrypt' library for password hashing
 const bcrypt = require('bcrypt');
 
+// Creating a new user schema using the mongoose Schema constructor
 const userSchema = new Schema({
   firstName: {
     type: String,
@@ -26,7 +31,7 @@ const userSchema = new Schema({
   },
 });
 
-// set up pre-save middleware to create password
+// Setting up pre-save middleware to hash the password before saving it to the database
 userSchema.pre('save', async function(next) {
   if (this.isNew || this.isModified('password')) {
     const saltRounds = 10;
@@ -36,11 +41,13 @@ userSchema.pre('save', async function(next) {
   next();
 });
 
-// compare the incoming password with the hashed password
+// Adding a method to the user schema to compare passwords
 userSchema.methods.isCorrectPassword = async function(password) {
   return await bcrypt.compare(password, this.password);
 };
 
+// Creating a User model based on the user schema
 const User = mongoose.model('User', userSchema);
 
+// Exporting the User model for use in other files
 module.exports = User;
