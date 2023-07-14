808dd6a58d1f9e1d86d7e659d707f05d3b6cbaec
CollectSpiders <mathewj519@gmail.com>
2023-07-13 19:03:22 -0700

    added comments explaining category.js

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,15 +1,21 @@
+// Import the 'mongoose' library
 const mongoose = require('mongoose');
 
+// Destructure the 'Schema' object from mongoose
 const { Schema } = mongoose;
 
+// Create a new schema for the 'category' collection
 const categorySchema = new Schema({
+  // Define the 'name' field with the following properties:
   name: {
-    type: String,
-    required: true,
-    trim: true
+    type: String,     // The field should be of type String
+    required: true,   // The field is required (cannot be empty)
+    trim: true        // Any leading or trailing whitespace will be trimmed
   }
 });
 
+// Create a model named 'Category' using the 'categorySchema'
 const Category = mongoose.model('Category', categorySchema);
 
+// Export the 'Category' model so that it can be used in other files
 module.exports = Category;
